c795e84ea9ea778e18d551947047fdd62874136c
Dimo Mitev <dimosoftuni@gmail.com>
2026-05-21 21:52:53 +0300

    add java class

---
+++ calculator.java
@@ -0,0 +1,36 @@
+public class Calculator {
+
+    // Addition
+    public double add(double a, double b) {
+        return a + b;
+    }
+
+    // Subtraction
+    public double subtract(double a, double b) {
+        return a - b;
+    }
+
+    // Multiplication
+    public double multiply(double a, double b) {
+        return a * b;
+    }
+
+    // Division
+    public double divide(double a, double b) {
+        if (b == 0) {
+            throw new ArithmeticException("Cannot divide by zero");
+        }
+        return a / b;
+    }
+
+    // Main method for testing
+    public static void main(String[] args) {
+
+        Calculator calc = new Calculator();
+
+        System.out.println("Add: " + calc.add(10, 5));
+        System.out.println("Subtract: " + calc.subtract(10, 5));
+        System.out.println("Multiply: " + calc.multiply(10, 5));
+        System.out.println("Divide: " + calc.divide(10, 5));
+    }
+}
